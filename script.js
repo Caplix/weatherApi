f3cf49f9a1923f11db44f416bb04fd9325d4d57b
Kristoffer Myhre <112164718+Caplix@users.noreply.github.com>
2023-09-20 06:46:37 +0200

    endret time values på grafen

--- script.js
+++ script.js
@@ -77,9 +77,16 @@ locations.forEach(location => {
   
     const ctx = document.getElementById("hourlyTempChart").getContext("2d");
   
-    const labels = hourlyTemp.map((_, index) => `Hour ${index}`);
+    const timeLabels = [
+      "00:00-01:00", "01:00-02:00", "02:00-03:00", "03:00-04:00",
+      "04:00-05:00", "05:00-06:00", "06:00-07:00", "07:00-08:00",
+      "08:00-09:00", "09:00-10:00", "10:00-11:00", "11:00-12:00",
+      "12:00-13:00", "13:00-14:00", "14:00-15:00", "15:00-16:00",
+      "16:00-17:00", "17:00-18:00", "18:00-19:00", "19:00-20:00",
+      "20:00-21:00", "21:00-22:00", "22:00-23:00", "23:00-00:00"
+    ];
     const data = {
-      labels: labels,
+      labels: timeLabels,
       datasets: [
         {
           label: "Hourly Temperature (°C)",
